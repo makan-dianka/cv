a63b217ac436f7cc2170da07dabdd1628db62eeb
Makan DIANKA <makan.dianka@hotmail.com>
2023-06-08 23:12:43 +0200

    get username from dotenv

--- cvapp/views/cvtheque.py
+++ cvapp/views/cvtheque.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .. models.cv import CvTheque
-from .. models.cv import Job
 from django.contrib.auth.models import User
+import os
 
 def cvtheque(request):
-    user = User.objects.get(username='makan')
+    user = User.objects.get(username=os.getenv('username'))
     cv = CvTheque.objects.filter(owner=user).last()
     return render(request, 'cvapp/cvtheque.html', {'cv' : cv})
