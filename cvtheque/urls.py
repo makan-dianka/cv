e3319a6d007c318957076050e8c43d7d32dc9166
Makan DIANKA <makan.dianka@hotmail.com>
2023-06-08 16:36:07 +0200

    include static dom settings

--- cvtheque/urls.py
+++ cvtheque/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('ama/', admin.site.urls),
     path('', include('cvapp.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
