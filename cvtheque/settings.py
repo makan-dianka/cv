5f1271ead0a6527b4612a5191bcbb8a6be9abe45
Makan DIANKA <makan.dianka@hotmail.com>
2023-06-10 17:56:35 +0200

    deploy settings (secure_cookie, session ...)

--- cvtheque/settings.py
+++ cvtheque/settings.py
@@ -126,3 +126,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# HTTPS settings 
+# set all the values to True in production
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
+SECURE_SSL_REDIRECT=True
